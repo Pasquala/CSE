b6a9be7d5f809c3896f19ebbea0cb12f679ea694
Pasquala <31043427+Pasquala@users.noreply.github.com>
2017-11-28 09:29:07 -0800

    Initial commit

---
+++ Notes.py
@@ -0,0 +1,19 @@
+# print('Try to figure out how this works')
+# print(13 % 5)
+
+# the "%" sign is a modulus. It finds the remainder.
+
+car_name = 'Weibe Mobile'
+car_type = 'BMW'
+car_cylinders = 8
+car_mpg = 5000.0
+
+print("I have a car called %s. It's pretty nice" % car_name)
+print("I have a car called %s. It's a %s" % (car_name, car_type))
+
+print("What is your name?")
+name = input(">_ ")
+print("Hello %s" % name)
+
+age = input("How old are you?")
+print("%s?! That's really old. You belong in a retirement home." % age)
