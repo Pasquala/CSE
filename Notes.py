bfc5e38589508afa383b7ff24ca0c0f899ee680b
Pasquala <31043427+Pasquala@users.noreply.github.com>
2017-11-29 09:49:07 -0800

    There are definitely turtles in this one!
    Nope no turtles D:
    
    There are cheeseburgers tho

--- Notes.py
+++ Notes.py
@@ -1,43 +1,110 @@
-print('Hello world')
+# print('Hello world')
 
-#Jorge Guerrero
+# Jorge Guerrero
 
-a = 4
-b = 3
-
-print(3 + 5)
-print(5 - 3)
-print(3 * 5)
-print(6 / 2)
-print(3 ** 2)
-
-print()
-print('Try to figure out how this works')
-print(13 % 5)
+# b = 3
+# a = 4
+#
+# print(3 + 5)
+# print(5 - 3)
+# print(3 * 5)
+# print(6 / 2)
+# print(3 ** 2)
+#
+# print()
+# print('Try to figure out how this works')
+# print(13 % 5)
 
 # the "%" sign is a modulus. It finds the remainder.
 
-car_name = 'Weibe Mobile'
-car_type = 'BMW'
-car_cylinders = 8
-car_mpg = 5000.0
+# car_name = 'Weibe Mobile'
+# car_type = 'BMW'
+# car_cylinders = 8
+# car_mpg = 5000.0
 
-print("I have a car called %s. It's pretty nice" % car_name)
-print("I have a car called %s. It's a %s" % (car_name, car_type))
+# print("I have a car called %s. It's pretty nice" % car_name)
+# print("I have a car called %s. It's a %s" % (car_name, car_type))
 
-print("What is your name?")
-name = input(">_ ")
-print("Hello %s" % name)
+# print("What is your name?")
+# name = input(">_ ")
+# print("Hello %s" % name)
 
-age = input("How old are you?")
-print("%s?! That's really old. You belong in a retirement home." % age)
+# age = input("How old are you?")
+# print("%s?! That's really old. You belong in a retirement home." % age)
 
-print('cheesebugers or hamburgers?')
-answer = input('>_')
-if answer == 'cheeseburgers':
-    then: print('Correct')
+# print('cheeseburgers or hamburgers?')
+# answer = input('>_')
+# if answer == 'cheeseburgers':
+#     then: print('Correct')
 
-elif answer == 'hamburgers':
-    then: print '>_> what is wrong with you'
+# elif answer == 'hamburgers':
+#     then: print '>_> what is wrong with you'
 
-else: print('I said cheesebugers or hamburgers >:C')
+# else: print('I said cheeseburgers or hamburgers >:C')
+
+# Function
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+# def print_hw():
+#     print('Hello World.')
+#     print('Enjoy the day.')
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# print_hw()
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+# def say_hi(name): # Name is a "parameter"
+#     print('Hello %s' % name)
+#     print('Coding is great!')
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# say_hi('Pasquala')
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+# def print_age(name, age):
+#     print('%s is %d years old' % (name, age))
+#     age += 1
+#     print('Next year, %s will be %d years old' % (name, age))
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# print_age('Pasquala', 14)
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+# def algebra_hw(x):
+#     return x**3 + 4*x**2 +7 * x - 4
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# print(algebra_hw(3))
+# print(algebra_hw(4))
+# print(algebra_hw(5))
+# print(algebra_hw(6))
+# print(algebra_hw(7))
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+def grade_calc(percentage):
+    if percentage >= 90:
+        return 'A'
+    elif percentage >= 80:
+        return 'B'
+    elif percentage >= 70:
+        return 'C'
+    elif percentage >= 60:
+        return 'D'
+    else:
+        return 'F'
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+print (grade_calc(100))
+
+print('HERE AM UR TURTLESSSSSSSSSSSSSSSSSSSSSSSSSSSSS')
+
