53d5c3923e70272c6dfae7426f111019cb5d9612
Pasquala <31043427+Pasquala@users.noreply.github.com>
2017-12-01 10:40:45 -0800

    Oh god why send help why is this so dummbbbbbbbbb and brrroooookkkkkeeeeeennnnnnn
    12/1/2017
    send help it broked and no there turtles. or cheeseburgers.
    
    yet.

--- Jorge Guerrero- Guess Game.py
+++ Jorge Guerrero- Guess Game.py
@@ -3,20 +3,26 @@ import sys
 
 winning_number = random.randint(1, 50)
 guesses = 5
+test_numerooooooo = winning_number
+print(test_numerooooooo)
+player_number = input('Choose a number between 1 and 50')
 
 
-def compare(player_number, guesses):
-    if guesses <= 0:
-
-    player_number = input('Choose a number between 1 and 50')
-    if player_number == winning_number:
+def compare(guess, lives):
+    if lives <= 0:
+        sys.exit(0)
+    if guess == winning_number:
         print('You win! the number was %d and you had %d guesses left.' % (winning_number, guesses))
         sys.exit(0)
-    elif player_number > winning_number:
-        guesses -= 1
-        print('Your number is too high! You now have %d guesses left.' % guesses)
+    elif guess > winning_number:
+        lives -= 1
+        print('Your number is too high! You now have %d guesses left.' % lives)
         compare(player_number, guesses)
-    elif player_number < winning_number:
-        guesses -=1
-        print('Your number is too low! You now have %d guesses left.' % guesses)
+    elif guess < winning_number:
+        lives -=1
+        print('Your number is too low! You now have %d guesses left.' % lives)
         compare(player_number,guesses)
+
+
+compare(player_number, guesses)
+
