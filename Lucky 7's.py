418b4ca7caecbf8d1763364d7245a9c8976d60a4
Pasquala <31043427+Pasquala@users.noreply.github.com>
2018-01-10 09:47:56 -0800

    Lucky 7's is done!
    lucky 7's is now done and complete, next up is blackjack
    :v man whats up with all these gambling games are we opening a casino?
    1/10/18

--- Lucky 7's.py
+++ Lucky 7's.py
@@ -4,11 +4,16 @@ dice1 = 0
 dice2 = 0
 money = 15
 rounds = 0
+max_round = 0
+max_money = 0
 
 
 def gamble():
-    global money, rounds, dice1, dice2
+    global money, rounds, dice1, dice2, max_money, max_round
     if money > 0:
+        if max_money < money:
+            max_money = money
+            max_round = rounds
         rounds += 1
         money -= 1
         dice1 = random.randint(1,6)
@@ -20,7 +25,8 @@ def gamble():
         elif dice1 + dice2 != 7:
             print('The number is %d. Ya lost. You have played %d rounds and have $%d.' % (dice1 + dice2, rounds, money))
             gamble()
-    else: print('Ya lost now git lost! It only took you %d games to be broke.' % rounds)
+    else: print('Ya lost now git lost! It only took you %d games to be broke. '
+                'Yer highest amount of money was $%d at round %d' % (rounds, max_money, max_round))
 
 
 gamble()
