6a84896e753b22ce73d3835c32105a3c2450d69e
Pasquala <31043427+Pasquala@users.noreply.github.com>
2018-01-12 10:29:23 -0800

    Blackjack makes absoluetly zero sense
    like how?!?!?!?!?
    send halp plz?
    1/12/18

--- Blackjack-JORGE GUERRERO.py
+++ Blackjack-JORGE GUERRERO.py
@@ -2,6 +2,16 @@ import random
 
 player_val = 0
 dealer_val = 0
+deck = {'h_ace': '1', 'h_2': '2', 'h_3': '3'}
 
-def play():
-    global player_val, dealer_val
+def hit():
+    global player_val, dealer_val, deck
+
+
+def stay():
+    global player_val, dealer_val, deck
+
+
+while player_val < 22 and dealer_val < 22:
+    if player_val == 21 or dealer_val == 21:
+        input("will you HIT or STAY ?")
