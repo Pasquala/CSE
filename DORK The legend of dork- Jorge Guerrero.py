3d8ec35082d5c57b592c1f04da33a282bf033110
Pasquala <31043427+Pasquala@users.noreply.github.com>
2018-02-28 09:48:34 -0800

    Updated DORK
    :D DORK has been updated with more rooms
    2/28/18

--- DORK The legend of dork- Jorge Guerrero.py
+++ DORK The legend of dork- Jorge Guerrero.py
@@ -25,16 +25,45 @@ meadowentrance = 'The meadow starts fading into an open field. ' \
                  'In the far north you see a building? ' \
                  'To the west you see nothing but an impassable forest, ' \
                  'south there is the meadow you woke up at, \nand to the east you see a what might be a lake.'
+lake = 'You look around and see a massive lake with a strange rock in the south part of the pond. ' \
+       'The lake is crystal clear and you can clearly look at your reflection.\n' \
+       'You might be able to swim to the strange rock'
+lakerock = 'You hop into the lake with a big splash and swim to the rock in the lake. ' \
+           'When you reach it, you drag yourself onto a smaller one nearby and catch your breath.\nYou get up and ' \
+           'look around the rock, feeling its smooth yet rough texture before spotting a hole. ' \
+           'Inside you can see something shining and see the bottom.'
+lakecave = 'You look into the hole as you slide into it, ending up into a medium-sized cave with beautiful crystals ' \
+           'lining the walls and glistening in the light.\n' \
+           'In the center you see a bag of gold. Above you is the hole you came from.'
 
 # MEADOW AREA OBJECTS START HERE!!!
 QUIETMEADOW = Room('Quiet Meadow Valley', quietmeadow, 'MEADOWENTRANCE', '', '', '', '', '')
 MEADOWENTRANCE = Room('Meadow Entrance', meadowentrance, 'SOUTHFIELDS', 'LAKE', 'QUIETMEADOW', '', '', '')
+LAKE = Room('Mirror lake', lake, '', '', 'LAKEROCK', 'MEADOWENTRANCE', '', '')
+LAKEROCK = Room('Strange Rock Island', lakerock, 'LAKE', '', '', '', '', 'LAKECAVE')
+LAKECAVE = Room('Crystal Cave', lakecave, '', '', '', '', 'LAKEROCK', '')
+
+# FIELDS AREA STARTS HERE!!!
+
+# FIELDS DESCRIPTIONS START HERE!!!
+southfields = 'You stumbled into a massive field! all around are little shiny objects such as rocks, ' \
+              'you look north to see a mansion and more field to the west, leading into a dense forest.\n' \
+              'To the east you can see the field leading into another forest but, this forest seems dead and desolate'
+westfields = 'You stand at the edge of a really green forest, you notice a considerably sturdy stick. ' \
+             'You look deeper into the forest and very easily hear the sound of a river rushing.\n' \
+             'To the north, you can go behind the house. ' \
+             'To the south you can return to the south of the fields or, you can go venture into the forest.'
+# FIELDS OBJECTS START HERE!!!
+SOUTHFIELDS = Room('Fields of Exploration, South', southfields, 'MANSIONENTRANCE', 'EASTFIELDS', 'MEADOWENTRANCE',
+                   'WESTFIELDS', '', '')
+WESTFIELDS = Room('Fields of Exploration, West', westfields, 'BEHINDHOUSE', '', 'SOUTHFIELDS', 'LUSHENTRANCE', '', '')
 
 current_node = QUIETMEADOW
 directions = ['NORTH', 'EAST', 'SOUTH', 'WEST', 'UP', 'DOWN']
 
 while True:
     print(current_node.name)
+    print(current_node.description)
     command = input('>_')
     if command == 'quit':
         quit(0)
