27cbf84221dc1321240986a414ad55152685a136
Pasquala <31043427+Pasquala@users.noreply.github.com>
2018-03-06 09:46:26 -0800

    Mansion is started!
    Imma stop being dumb and imma write out whats in there before actually filling it in thatway i can think about how it fits together
    3/6/18

--- DORK The legend of dork- Jorge Guerrero.py
+++ DORK The legend of dork- Jorge Guerrero.py
@@ -45,6 +45,7 @@ LAKE = Room('Mirror lake', lake, '', '', 'LAKEROCK', 'MEADOWENTRANCE', '', '')
 LAKEROCK = Room('Strange Rock Island', lakerock, 'LAKE', '', '', '', '', 'LAKECAVE')
 LAKECAVE = Room('Crystal Cave', lakecave, '', '', '', '', 'LAKEROCK', '')
 
+
 # FIELDS AREA STARTS HERE!!!
 
 # FIELDS DESCRIPTIONS START HERE!!!
@@ -55,23 +56,60 @@ westfields = 'You stand at the edge of a really green forest, you notice a consi
              'You look deeper into the forest and very easily hear the sound of a river rushing.\n' \
              'To the north, you can go behind the house. ' \
              'To the south you can return to the south of the fields or, you can go venture into the forest.'
+eastfields = 'You stand at the edge of a thin forest, most the trees are dead and rotting. Though, parts of the ' \
+             'forest are growing back and you can go east to venture in.\n' \
+             'To the north you can go behind the mansion. To the south, you can travel back to the fields in the south.'
 mansionentrance = 'Walking up the the mansion you see its intricate designs but see something more peculiar, ' \
                   'the door is open enough that you can walk in.\n' \
                   'By peeping through the open door you see a courtyard leading to the actual house which is a ' \
                   'little farther north.\n' \
                   'To the west you see the dense forest and can hear some sound coming from that area albeit faint. ' \
                   'To the east you can see more of the desolate forest.'
+behindhouse = ''
 # FIELDS OBJECTS START HERE!!!
 SOUTHFIELDS = Room('Fields of Exploration, South', southfields, 'MANSIONENTRANCE', 'EASTFIELDS', 'MEADOWENTRANCE',
                    'WESTFIELDS', '', '')
 WESTFIELDS = Room('Fields of Exploration, West', westfields, 'BEHINDHOUSE', '', 'SOUTHFIELDS', 'LUSHENTRANCE', '', '')
-E
+EASTFIELDS = Room('Fields of Exploration, East', eastfields, 'BEHINDHOUSE', 'DESOLATEENTRANCE', 'SOUTHFIELDS', '', '',
+                  '')
 MANSIONENTRANCE = Room('Mansion Entrance', mansionentrance, 'COURTYARD', '', 'SOUTHFIELDS', '', '', '')
+BEHINDHOUSE = Room('Behind The House', behindhouse, '', 'EASTFIELDS', '', 'WESTFIELDS', '', '')
 
+
+# MANSION AREA STARTS HERE!!!
+
+# MANSION F1 DESCRIPTIONS START HERE!!!
+courtyard = ''
+mainroom = ''
+diningroom = ''
+kitchen = ''
+livingroom = ''
+garden = ''
+trophyroom = ''
+
+# MANSION F1 OBJECTS START HERE!!!
+COURTYARD = Room()
+MAINROOM = Room()
+DININGROOM = Room()
+KITCHEN = Room()
+LIVINGROOM = Room()
+GARDEN = Room()
+TROPHYROOM = Room()
+
+# MANSION F2 DESCRIPTIONS START HERE!!!
+
+# MANSION F2 OBJECTS START HERE!!!
+
+# MANSION B1 DESCRIPTIONS START HERE!!!
+
+# MANSION B1 OBJECTS START HERE!!!
+
+# CONTROLLER CODE IS HERE!!!
 
 current_node = QUIETMEADOW
 directions = ['north', 'east', 'south', 'west', 'up', 'down']
 short_directions = ['n', 'e', 's', 'w', 'u', 'd']
+
 
 while True:
     print(current_node.name)
@@ -81,6 +119,7 @@ while True:
     if command == 'quit':
         quit(0)
     elif command == 'cheeseburger':
+        if current_node == TROPHYROOM
         print('DON\'T TRUST THE DUCKS!!!')
     elif command == 'fly':
         print('You cannot fly seeing as you are not a bird!')
