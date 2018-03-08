8d18da1aa6101f99db1fa61ade4a35377efdfee6
Pasquala <31043427+Pasquala@users.noreply.github.com>
2018-03-08 09:48:40 -0800

    Haavee is nearing completion!
    I need to do the dialogue and other stuff like that and i have most the attributes done
    3/8/18

--- DORK The legend of dork Characters- Jorge Guerrero.py
+++ DORK The legend of dork Characters- Jorge Guerrero.py
@@ -1,15 +1,25 @@
 class Chupadore(object):
-    def __init__(self, name, description, dialogue, item, happiness):
+    def __init__(self, name, description, dialogue, item, affinity):
         self.name = name
         self.description = description
         self.dialogue = dialogue
         self.item = item
-        self.happiness = happiness
+        self.affinity = affinity
 
 
 haavee = 'You look at one of the aliens, a "Chupadore", and what you see is short fluffy fur colored a light teal. He' \
          'has the regular blue recruit armor and he fiddles with a paper in his paws.\n' \
          'He looks very young compared to everyone else, about 12-13 human years.'
-haavee_dia = []
+haavee_dia = {'0': 'Oh! Um... Hello There sir? W-Why did a giant portal appear and why did you come through it?',
+              '1': 'W-well my name is Harvey, I\'m a blue recruit in this base',
+              '2': 'This base is uh... Somewhere in La-La-Lamanthia? Lamantha, Lemanthia?',
+              '3': 'Eeaarrtthh? Eaarth? what\'s an Eaarrrthhh? This is Sirca!',
+              '4': 'Sirca is a ring world, it is ruled by the house of Omega.',
+              '5': 'Am I alone here? No of course not! There is Chedder and Hartman and Dad and Pas and Re... Um...',
+              }
 
-HAAVEE = Chupadore('Harvey', haavee, haavee_dia, 'drawing', 85)
+drawing = 'The drawing is a somewhat crude drawing of Harvey and another being that looks drastically different. ' \
+          'On the back there is a heart and in the heart it says \'For Harv!\''
+
+HAAVEE = Chupadore('Harvey', haavee, haavee_dia, drawing, 'Blue')
+
